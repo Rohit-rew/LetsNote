9b1b7b4c602b342725d71f0b152b659a876b3d7a
rohit <rohit.rew@gmail.com>
2022-09-12 16:44:52 +0530

    made responsive - 1st attempt improved ui design - 1st attempt

--- src/note.js
+++ src/note.js
@@ -4,20 +4,31 @@ export default function Note (props){
 
     console.log(props.editable)
 
+    let style={
+
+        textDecoration : props.ischecked ? "line-through" : 'none',
+        color : props.ischecked ? "grey" : 'black',
+        textDecorationColor: "orange"
+    }
+
    
     return (
 
         <div className='notes'>
 
                 <input  onChange={(e)=>props.tickuntick(e , props.id)} className='checkbox' type="checkbox" checked={props.ischecked}/>
-                {!props.editable && <p onDoubleClick={(e)=>props.editnote(props.id)} style={{textDecoration : props.ischecked ? "line-through" : 'none'} } className='text'>{props.note}</p>}
+                {!props.editable && <p onDoubleClick={(e)=>props.editnote(props.id)} style={style} className='text'>{props.note}</p>}
 
                 {props.editable && <input onKeyUp={(e)=>props.setedit(e , props.id)} defaultValue={props.note}  className="editfield" type='text'/>}
-                <button onClick={(e)=>props.deletenote(e,props.id)}>D</button>
+                <button onClick={(e)=>props.deletenote(e,props.id)}><span>
+                    <img className="delimage" src={require("../src/delete.png")}/>
+                    </span></button>
 
         </div>
 
     )
 }
+
+// src={require('./logo.jpeg')}
 
  
